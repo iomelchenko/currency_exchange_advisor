5b40aea60f9f798dcc7d88d4a4fb7b04e7edec10
Igor Omelchenko <igomelch@gmail.com>
2017-04-28 12:17:07 +0300

    Added user_name validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,21 @@ describe User do
   end
 
   describe 'model validations' do
+    let!(:test_user) do
+      create :user, email: 'test@test.com', username: 'test_user'
+    end
+
+    let(:user_params) do
+      { password: '123456', password_confirmation: '123456' }
+    end
+
     it { should validate_uniqueness_of(:username) }
+
+    it 'should return already exists error' do
+      user_params.merge!(username: 'test@test.com', email: 'test_2@test.com')
+      user = User.create(user_params)
+      expect(user.errors[:username].first)
+        .to eql 'Username (as email) already exists'
+    end
   end
 end
